1304829fbac6fd4a45feb32e31ee62175b8984d6
eugeneonufran <eugeneonufran@gmail.com>
2024-08-14 20:31:27 +0300

    Add Docker override

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -10,6 +10,10 @@ builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
+builder.Services.AddMarten(opt =>
+{
+    opt.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
 var app = builder.Build();
 
 app.MapCarter();
