2a9c5218f078087d8d75c8c8fe6d0a89726ec1b7
eugeneonufran <eugeneonufran@gmail.com>
2024-08-28 21:36:25 +0300

    Implement pagination

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -1,16 +1,17 @@
 ﻿
 namespace Catalog.API.Products.GetProducts
 {
-    //public record GetProductsRequest();
+    public record GetProductsRequest(int? PageNumber, int? PageSize);
     public record GetProductsResponse(IEnumerable<Product> Products);
 
     public class GetProductsEndpoint : ICarterModule
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapGet("/products", async (ISender sender) =>
+            app.MapGet("/products", async ([AsParameters] GetProductsRequest request, ISender sender) =>
             {
-                var results = await sender.Send(new GetProductsQuery());
+                var query = request.Adapt<GetProductsQuery>();
+                var results = await sender.Send(query);
                 var response = results.Adapt<GetProductsResponse>();
 
                 return Results.Ok(response);
--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,13 +1,13 @@
-﻿
-namespace Catalog.API.Products.GetProducts
+﻿namespace Catalog.API.Products.GetProducts
 {
-    public record GetProductsQuery(): IQuery<GetProductsResult>;
+    public record GetProductsQuery(int? PageNumber, int? PageSize) : IQuery<GetProductsResult>;
     public record GetProductsResult(IEnumerable<Product> Products);
     public class GetProductsQueryHandler(IDocumentSession session) : IQueryHandler<GetProductsQuery, GetProductsResult>
     {
         public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
         {
-            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+            var products = await session.Query<Product>()
+                .ToPagedListAsync(query.PageNumber ?? 1, query.PageSize ?? 10, cancellationToken);
             return new GetProductsResult(products);
         }
     }
