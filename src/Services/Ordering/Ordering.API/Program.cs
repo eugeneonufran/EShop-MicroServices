e93d5b7b769538258eb29e70eb3bdafd02524351
eugeneonufran <eugeneonufran@gmail.com>
2024-10-24 11:33:14 +0300

    Initial setup

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,13 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-//add services
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
 var app = builder.Build();
 
 //configure http pipeline
