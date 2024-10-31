338c33ed08a24c8120a2a61c6c1ce7c77dd11b7a
eugeneonufran <eugeneonufran@gmail.com>
2024-10-31 22:37:08 +0200

    Add configuration for everything except Order

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Infrastructure.Data.Configurations;
+
+public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+{
+    public void Configure(EntityTypeBuilder<Customer> builder)
+    {
+        builder.HasKey(c=>c.Id);
+        builder.Property(c=>c.Id).HasConversion(customerId=>customerId.Value, dbid=>CustomerId.Of(dbid));
+        builder.Property(c => c.Name).HasMaxLength(255);
+        builder.HasIndex(c=>c.Email).IsUnique();
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Infrastructure.Data.Configurations;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.HasKey(c => c.Id);
+        builder.Property(c => c.Id).HasConversion(customerId => customerId.Value, dbid => ProductId.Of(dbid));
+        builder.Property(c => c.Name).HasMaxLength(255).IsRequired();
+    }
+}
