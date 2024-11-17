85ccbb8967dc24e93682d49c3c54f13dd336d6eb
eugeneonufran <eugeneonufran@gmail.com>
2024-11-17 14:54:04 +0200

    Add UpdateOrderCommand

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -1,9 +1,11 @@
 ﻿global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Exceptions;
 global using FluentValidation;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.Extensions.DependencyInjection;
 global using Ordering.Application.Data;
 global using Ordering.Application.DTOs;
+global using Ordering.Application.Exceptions;
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
 global using System.Reflection;
--- src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -1,14 +1,14 @@
 ﻿namespace Ordering.Application.Orders.Commands.CreateOrder;
 
-public class CreateOrderHandler(IApplicationDbContext context)
+public class CreateOrderHandler(IApplicationDbContext dbContext)
     : ICommandHandler<CreateOrderCommand, CreateOrderResult>
 {
     public async Task<CreateOrderResult> Handle(CreateOrderCommand command, CancellationToken cancellationToken)
     {
         var order = CreateNewOrder(command.Order);
 
-        context.Orders.Add(order);
-        await context.SaveChangesAsync(cancellationToken);
+        dbContext.Orders.Add(order);
+        await dbContext.SaveChangesAsync(cancellationToken);
 
         return new CreateOrderResult(order.Id.Value);
     }
@@ -16,11 +16,11 @@ public class CreateOrderHandler(IApplicationDbContext context)
     private Order CreateNewOrder(OrderDTO orderDTO)
     {
         var sa_DTO = orderDTO.ShippingAddress;
-        var va_DTO = orderDTO.BillingAddress;
+        var ba_DTO = orderDTO.BillingAddress;
         var p_DTO = orderDTO.Payment;
 
         var shippingAddress = Address.Of(sa_DTO.FirstName, sa_DTO.LastName, sa_DTO.EmailAddress, sa_DTO.AddressLine, sa_DTO.Country, sa_DTO.State, sa_DTO.ZipCode);
-        var billingAddress = Address.Of(va_DTO.FirstName, va_DTO.LastName, va_DTO.EmailAddress, va_DTO.AddressLine, va_DTO.Country, va_DTO.State, va_DTO.ZipCode);
+        var billingAddress = Address.Of(ba_DTO.FirstName, ba_DTO.LastName, ba_DTO.EmailAddress, ba_DTO.AddressLine, ba_DTO.Country, ba_DTO.State, ba_DTO.ZipCode);
         var payment = Payment.Of(p_DTO.CardName, p_DTO.CardNumber, p_DTO.Expiration, p_DTO.Cvv, p_DTO.PaymentMethod);
 
         var newOrder = Order.Create(
