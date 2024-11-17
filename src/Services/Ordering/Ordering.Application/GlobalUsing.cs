fc8455cd31a1e307baf6086c07133f2080eb9a65
eugeneonufran <eugeneonufran@gmail.com>
2024-11-18 01:15:41 +0200

    Add pagination

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -1,5 +1,6 @@
 ﻿global using BuildingBlocks.CQRS;
 global using BuildingBlocks.Exceptions;
+global using BuildingBlocks.Pagination;
 global using FluentValidation;
 global using MediatR;
 global using Microsoft.EntityFrameworkCore;
