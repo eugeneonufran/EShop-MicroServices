05b729b5509a6175f937d7fe051aa1d5806d0aec
eugeneonufran <45742247+eugeneonufran@users.noreply.github.com>
2024-12-06 19:44:01 +0200

    Add BasketCheckout endpoint

---
+++ src/Services/Basket/Basket.API/Basket/CheckoutBasket/CheckoutBasketEndpoints.cs
@@ -0,0 +1,20 @@
+﻿namespace Basket.API.Basket.CheckoutBasket;
+
+public record CheckoutBasketRequest(BasketCheckoutDTO BasketCheckoutDTO);
+public record CheckputBasketResponse(bool IsSuccess);
+public class CheckoutBasketEndpoints : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/basket/checkout", async (CheckoutBasketRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<CheckoutBasketCommand>();
+
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<CheckputBasketResponse>();
+
+            return Results.Ok(response);
+        });
+    }
+}
---
+++ src/Services/Basket/Basket.API/Basket/CheckoutBasket/CheckoutBasketHandler.cs
@@ -0,0 +1,37 @@
+﻿namespace Basket.API.Basket.CheckoutBasket;
+
+public record CheckoutBasketCommand(BasketCheckoutDTO BasketCheckoutDTO) : ICommand<CheckoutBasketResult>;
+
+public record CheckoutBasketResult(bool IsSuccess);
+
+public class CheckoutBasketCommandValidator : AbstractValidator<CheckoutBasketCommand>
+{
+    public CheckoutBasketCommandValidator()
+    {
+        RuleFor(x => x.BasketCheckoutDTO).NotNull().WithMessage("BasketCheckoutDTO cannot be null");
+        RuleFor(x => x.BasketCheckoutDTO.UserName).NotEmpty().WithMessage("UserName cannot be empty");
+    }
+}
+public class CheckoutBasketHandler
+    (IBasketRepository repository, IPublishEndpoint publishEndpoint)
+    : ICommandHandler<CheckoutBasketCommand, CheckoutBasketResult>
+{
+    public async Task<CheckoutBasketResult> Handle(CheckoutBasketCommand command, CancellationToken cancellationToken)
+    {
+        var userName = command.BasketCheckoutDTO.UserName;
+
+        var basket = await repository.GetBasketAsync(userName, cancellationToken);
+        if (basket == null)
+        {
+            return new CheckoutBasketResult(false);
+        }
+
+        var eventMessage = command.BasketCheckoutDTO.Adapt<BasketCheckoutEvent>();
+        eventMessage.TotalPrice = basket.TotalPrice;
+        await publishEndpoint.Publish(eventMessage, cancellationToken);
+
+        await repository.DeleteBasketAsync(userName, cancellationToken);
+
+        return new CheckoutBasketResult(true);
+    }
+}
