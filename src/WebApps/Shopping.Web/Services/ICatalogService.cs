584d54c7e42ad3ad77af7d65431daff576214782
eugeneonufran <45742247+eugeneonufran@users.noreply.github.com>
2024-12-08 16:13:02 +0200

    Add IcatalogService

---
+++ src/WebApps/Shopping.Web/Services/ICatalogService.cs
@@ -0,0 +1,8 @@
+﻿namespace Shopping.Web.Services;
+
+public interface ICatalogService
+{
+    Task<GetProductsResponse> GetProductsAsync(int? pageNumber = 1, int? pageSize = 10);
+    Task<GetProductByIdResponse> GetProductAsync(Guid id);
+    Task<GetProductByCategoryResponse> GetProductByCategoryAsync(string category);
+}
